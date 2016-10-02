b69955aa9341cee725ee6255b4d55fa0b0b57875
chengongliang <chengongliang7@gmail.com>
2016-10-02 15:34:50 +0800

    update

--- tool/parser.py
+++ tool/parser.py
@@ -16,6 +16,7 @@ class Parser_conf(object):
         latest_configs = self.url_request(request_type,url)
         latest_configs = json.loads(latest_configs)
         self.deploy_info.update(latest_configs)    
+        return self.deploy_info
 
     def url_request(self,action,url,**extra_data):
         abs_url = "http://%s:%s/%s" % (settings.configs['Server'],
@@ -27,11 +28,16 @@ class Parser_conf(object):
                 req = urllib2.Request(abs_url)
                 req_data = urllib2.urlopen(req,timeout=settings.configs['RequestTimeout'])
                 callback = req_data.read()
-                print "-->server response:",callback
+                #print "-->server response:",callback
                 return callback
             except urllib2.URLError,e:
                 exit("\033[31;1m%s\033[0m"%e)
 
 if __name__ == "__main__":
     p = Parser_conf()
-    p.load_configs()
+    info = p.load_configs()['project']
+    for pro in info:
+        deploy_dir = info[pro][0]
+        tpye = info[pro][1]
+        print "*" * 30
+        print deploy_dir,tpye
