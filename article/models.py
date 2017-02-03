766a6a91f30300b2e09bbcb05385c3457d5d4579
chengongliang <chengongliang7@gmail.com>
2017-02-03 14:01:29 +0800

    update

--- article/models.py
+++ article/models.py
@@ -1,6 +1,6 @@
 #_*_coding:utf8_*_
 from __future__ import unicode_literals
-
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
@@ -10,8 +10,15 @@ class Article(models.Model):
     date_time = models.DateTimeField(auto_now_add = True) #博客日期
     content = models.TextField(blank = True,null = True) #博客正文
 
+    #获取 URL 并转换成 url 的表示格式
+    def get_absolute_url(self):
+        path = reverse('detail',kwargs={'id':self.id})
+        return 'http://127.0.0.1:8000%s' %path
+
     def __unicode__(self):
         return self.title
 
     class Meta:
         ordering = ['-date_time']
+
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from article import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('deploy.api_urls')),
     #url(r'^blog/', include('article.urls')),
-    url(r'^$','article.views.home',name = 'home'),
-    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^$',article_views.home,name = 'home'),
+    url(r'^(?P<id>\d+)/$', article_views.detail, name='detail'),
+    url(r'^archives/$', article_views.archives, name = 'archives'),
+    url(r'^aboutme/$', article_views.about_me, name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', article_views.search_tag, name = 'search_tag'),
 ]
